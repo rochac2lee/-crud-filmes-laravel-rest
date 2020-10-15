38a95057fc1c6c2c42a0a47aeb02381d6b1bd069
rochac2lee <cleberli.rocha@gmail.com>
2020-10-15 10:39:22 -0300

    Correção da Navegação das páginas

--- resources/views/filme.blade.php
+++ resources/views/filme.blade.php
@@ -21,10 +21,10 @@
 <!-- dark -->
 <ul class="nav nav-tabs bg-dark animated fadeInDown">
   <li class="nav-item">
-    <a class="nav-link" href="home">Catálogo</a>
+    <a class="nav-link" href="../../home">Catálogo</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link active" href="filmes">Novo</a>
+    <a class="nav-link active" href="../../filmes">Novo</a>
   </li>
 </ul>
 
